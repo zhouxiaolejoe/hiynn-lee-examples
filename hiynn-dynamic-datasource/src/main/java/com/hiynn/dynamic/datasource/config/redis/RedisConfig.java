5ac78916b45dd0fc53d3cd5fcd8a225074aae6dc
zhouxiaolejoe <a864511019@163.com>
2019-07-19 10:56:33 +0800

    hiynn-lee-examples update

--- hiynn-dynamic-datasource/src/main/java/com/hiynn/dynamic/datasource/config/redis/RedisConfig.java
+++ hiynn-dynamic-datasource/src/main/java/com/hiynn/dynamic/datasource/config/redis/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.hiynn.dynamic.datasource.config;
+package com.hiynn.dynamic.datasource.config.redis;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
