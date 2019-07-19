c1db73424821f43aaa3cfaad71662b33337bfc33
zhouxiaolejoe <a864511019@163.com>
2019-07-19 11:05:20 +0800

    hiynn-lee-examples update

---
+++ hiynn-dynamic-datasource/src/main/java/com/hiynn/dynamic/datasource/config/websocket/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.hiynn.dynamic.datasource.config.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+* @Description  
+* @Author ZhouXiaoLe
+* @Date  2019/7/19  11:04
+* @Param 
+* @return 
+**/
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/socket");
+        registry.enableSimpleBroker("/topic");
+    }
+}
