3613def808a64cb3bc4c3675a717b037cd9ac429
zhouxiaolejoe <a864511019@163.com>
2019-07-20 12:11:34 +0800

    hiynn-lee-examples update

---
+++ hiynn-dynamic-datasource/src/test/java/com/hiynn/dynamic/datasource/generator/mapper/TUserInfoMapperTest.java
@@ -0,0 +1,17 @@
+package com.hiynn.dynamic.datasource.generator.mapper;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+/**
+ * Created by ZhouXiaoLe on 2019/7/20
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@Slf4j
+public class TUserInfoMapperTest {
+
+}
