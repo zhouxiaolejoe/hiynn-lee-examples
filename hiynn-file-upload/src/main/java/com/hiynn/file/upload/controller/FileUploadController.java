41e7c2e5102d0b9b0f88979015d0b4e62967bb23
zhouxiaolejoe <a864511019@163.com>
2019-07-20 22:30:46 +0800

    update

--- hiynn-file-upload/src/main/java/com/hiynn/file/upload/controller/FileUploadController.java
+++ hiynn-file-upload/src/main/java/com/hiynn/file/upload/controller/FileUploadController.java
@@ -1,42 +1,90 @@
 package com.hiynn.file.upload.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import com.hiynn.file.upload.untils.ResultBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.UUID;
 
 /**
  * Created by ZhouXiaoLe on 2019/7/20
  */
-@Controller
+@RestController
+@Slf4j
 public class FileUploadController {
-	@RequestMapping("/upload")
-	@ResponseBody
-	public String handleFileUpload(@RequestParam("file") MultipartFile file){
-		if(!file.isEmpty()){
-			try {
-				BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(new File(file.getOriginalFilename())));
-				out.write(file.getBytes());
-				out.flush();
-				out.close();
-			}catch(FileNotFoundException e) {
-				e.printStackTrace();
-				return "上传失败,"+e.getMessage();
-			}catch (IOException e) {
-				e.printStackTrace();
-				return "上传失败,"+e.getMessage();
-			}
+	@Value("${upload.file.path}")
+	public String uploadPath;
 
-			return "上传成功";
+	@PostMapping("/upload")
+	public ResultBuilder uploadFile(HttpServletRequest request, @RequestParam("file") MultipartFile file) throws IOException {
+		return  ResultBuilder.success(FileUtil(request,file));
+	}
 
-		}else{
-
-			return "上传失败，因为文件是空的.";
-
+	@PostMapping("/uploads")
+	public ResultBuilder uploadsFile(HttpServletRequest request, @RequestParam("files") MultipartFile[] multipartFile) throws IOException {
+		ArrayList<Object> list = new ArrayList<>();
+		for (MultipartFile file : multipartFile) {
+			list.add(FileUtil(request,file));
 		}
+		return  ResultBuilder.success(list);
+	}
+
+	@GetMapping("/download/{fileName}")
+	public ResultBuilder downloadFile(HttpServletResponse response,
+	                                  @PathVariable String fileName){
+		File file = new File(uploadPath + fileName);
+		InputStream stream = null;
+		OutputStream outputStream = null;
+		try {
+			fileName = new String(fileName.getBytes(),"ISO-8859-1");
+			response.setCharacterEncoding("utf-8");
+			response.setContentType("application/json");
+			response.setHeader("Content-Disposition", "attachment;fileName=" + fileName);
+
+			stream = new FileInputStream(file);
+			outputStream = response.getOutputStream();
+			byte[] bytes = new byte[1024];
+			int lenth;
+			while ((lenth=stream.read(bytes))>0){
+				outputStream.write(bytes,0,lenth);
+			}
+		} catch (IOException e) {
+			return ResultBuilder.fail("文件下载失败:"+e.getMessage());
+		} finally {
+			try {
+				outputStream.close();
+			} catch (IOException e) {
+				return ResultBuilder.fail("文件下载失败:"+e.getMessage());
+			}
+			try {
+				stream.close();
+			} catch (IOException e) {
+				return ResultBuilder.fail("文件下载失败:"+e.getMessage());
+			}
+		}
+		return  ResultBuilder.success();
+	}
+	private String FileUtil(HttpServletRequest request,MultipartFile file) throws IOException {
+		//生成文件名
+		String filePrefix = UUID.randomUUID().toString().replaceAll("-", "");
+		//获取文件后缀
+		String fileSuffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+		String filePath = String.format("%s%s%s", uploadPath, filePrefix, fileSuffix);
+		FileUtils.copyInputStreamToFile(file.getInputStream(),new File(filePath));
+//		file.transferTo(new File("C:\\var\\log\\1.txt"));
+		String basePath = String.format("%s%s%s%s%s%s%s",
+				request.getScheme(),"://",
+				request.getServerName(),":",
+				request.getServerPort(),
+				request.getContextPath(),"/");
+		return String.format("%s%s%s%s", basePath, "image/", filePrefix, fileSuffix);
 	}
 
 }
---
+++ hiynn-file-upload/src/main/java/com/hiynn/file/upload/untils/CustomPageInfo.java
@@ -0,0 +1,42 @@
+package com.hiynn.file.upload.untils;
+
+import com.github.pagehelper.PageInfo;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+/**
+* @Description  用于前端分页返回
+* @Author ZhouXiaoLe
+* @Date  2019/7/17  16:28
+* @Param 
+* @return 
+**/
+@Getter
+@Setter
+@ToString
+public class CustomPageInfo<T> {
+    private List<T> list;
+    private InnerPageInfo pageInfo = new InnerPageInfo();
+
+    public static <T> CustomPageInfo<T> of(PageInfo<T> pageInfo) {
+        CustomPageInfo<T> customPageInfo = new CustomPageInfo<>();
+        customPageInfo.list = pageInfo.getList();
+        customPageInfo.pageInfo.currentPage = pageInfo.getPageNum();
+        customPageInfo.pageInfo.totalCount = pageInfo.getTotal();
+        customPageInfo.pageInfo.pageSize = pageInfo.getSize();
+        return customPageInfo;
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    public class InnerPageInfo {
+        int currentPage;
+        long totalCount;
+        long pageSize;
+    }
+
+}
