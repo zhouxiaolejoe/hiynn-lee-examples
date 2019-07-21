1bf149ece31f370eb6c1445880ccd4c5d502e7e8
zhouxiaolejoe <a864511019@163.com>
2019-07-21 20:18:04 +0800

    update

---
+++ hiynn-spring-admin/hiynn-admin-client/src/main/java/com/hiynn/admin/client/HiynnAdminClientApplication.java
@@ -0,0 +1,13 @@
+package com.hiynn.admin.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiynnAdminClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiynnAdminClientApplication.class, args);
+	}
+
+}
---
+++ hiynn-spring-admin/hiynn-admin-server/src/main/java/com/hiynn/admin/server/HiynnAdminServerApplication.java
@@ -0,0 +1,15 @@
+package com.hiynn.admin.server;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class HiynnAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiynnAdminServerApplication.class, args);
+	}
+
+}
