1388f6e4e7d84fa9658a441480d7d47dc2e4b28e
zhouxiaolejoe <a864511019@163.com>
2019-07-23 11:54:24 +0800

    update

---
+++ hiynn-spring-security/src/main/java/com/hiynn/spring/security/HiynnSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.hiynn.spring.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiynnSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiynnSpringSecurityApplication.class, args);
+	}
+
+}
